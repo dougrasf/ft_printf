d92619bb96db3cfd069ae5ff7eb3cd0d9526af9d
dofranci <dofranci@student.42sp.org.br>
2022-08-01 22:29:18 -0300

    draftok

--- ft_printf.c
+++ ft_printf.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <unistd.h>
 #include <stdarg.h>
 
@@ -6,128 +5,175 @@
 
 int ft_putchar(char c)
 {
-    write(1, &c, 1);
-    return (1);
+	write(1, &c, 1);
+	return (1);
 }
 
 int ft_putnbr2(unsigned int n)
 {
-    int ret;
+	int ret;
 
-    ret = 0;
-    if (n >= 10)
-    {
-        ret += ft_putnbr2(n / 10);
-        ret += ft_putchar(n % 10 + '0');
-    }
-    else
-        ret += ft_putchar(n + '0');
-    return (ret); 
+	ret = 0;
+	if (n >= 10)
+	{
+		ret += ft_putnbr2(n / 10);
+		ret += ft_putchar(n % 10 + '0');
+	}
+	else
+		ret += ft_putchar(n + '0');
+	return (ret); 
 }
 
 int ft_putnbr(int n)
 {
-    int ret;
+	int ret;
 
-    ret = 0;
-    if (n < 0)
-        {
-            write(1, "-", 1);
-            ret++;
-            n *= -1;
-        }
-    return(ft_putnbr2(n) + ret);
+	ret = 0;
+	if (n < 0)
+		{
+			write(1, "-", 1);
+			ret++;
+			n *= -1;
+		}
+	return(ft_putnbr2(n) + ret);
 }
 
 int ft_putstr(char *s)
 {
-    int ret;
-    int i;
+	int ret;
+	int i;
 
-    ret = 0;
-    i = -1;
-    if (!s)
-        s = "(null)";
-    while(s[++i])
-    {
-        write(1, &s[i], 1);
-        ret++;
-    }
-    return (ret);
+	ret = 0;
+	i = -1;
+	if (!s)
+		s = "(null)";
+	while(s[++i])
+	{
+		write(1, &s[i], 1);
+		ret++;
+	}
+	return (ret);
 }
 
-int print_p(unsigned long int)
+int ft_putptr2(unsigned long int n)
 {
-    int ret;
+	int ret;
 
-    ret = 0;
-    
-    return(ret);
+	ret = 0;
+	if(n >= 16)
+	{
+		ret += ft_putptr2(n / 16);
+		ret += ft_putptr2(n % 16);
+	}
+	else if(n <= 9)
+		ret += ft_putchar(n + '0');
+	else 
+		ret += ft_putchar(n - 10 + 'a');
+	return(ret);
+}
+
+int ft_putptr(unsigned long long int n)
+{
+	int ret;
+
+	ret = 0;
+	ret += write(1, "0x", 2);
+	if (n == 0)
+		ret += write(1, "0", 1); // verificar
+	else
+	   ret += ft_putptr2(n);
+	return (ret);
+}
+
+int ft_putx(unsigned int n, char conv)
+{
+	int ret;
+
+	ret = 0;
+	if(n >= 16)
+	{
+		ret += ft_putx(n / 16, conv);
+		ret += ft_putx(n % 16, conv);
+	}
+	else if(n <= 9)
+		ret += ft_putchar(n + '0');
+	else if(conv == 'x')
+		ret += ft_putchar(n - 10 + 'a');
+	else if(conv == 'X')
+		ret += ft_putchar(n - 10 + 'A');
+	return(ret);
 }
 
 int ft_printer(char  conv, va_list *ap)
 {
-    int ret;
+	int ret;
 
-    ret = 0;
+	ret = 0;
 	if (conv == '%')
 	{	
-        write(1, "%", 1);
-        ret++;
-    }
-    if (conv == 'c')
+		write(1, "%", 1);
+		ret++;
+	}
+	if (conv == 'c')
 		ret += ft_putchar(va_arg(*ap, int));
 	if (conv == 's')
 		ret += ft_putstr(va_arg(*ap, char *));
-    if (conv == 'd' || conv == 'i')
+	if (conv == 'd' || conv == 'i')
 		ret += ft_putnbr(va_arg(*ap, int));
 	if (conv == 'u')
 		ret += ft_putnbr2(va_arg(*ap, unsigned int));
 	if (conv == 'p')
-		ret += print_p(va_arg(*ap, unsigned long int));
-	/*if (conv == 'x' || conv == 'X')
-		ret += print_h(va_arg(*ap, unsigned int), conv);
-    */
+		ret += ft_putptr(va_arg(*ap, unsigned long int));
+	if (conv == 'x' || conv == 'X')
+		ret += ft_putx(va_arg(*ap, unsigned int), conv);
 	return (ret);
 }
 
 int ft_printf(const char *format, ...)
 {
-    int ret;
-    int i;
-    va_list ap;
+	int ret;
+	int i;
+	va_list ap;
 
-    ret = 0;
-    i = 0;
-    va_start(ap, format);
-    while(format[i])
-    {
-        if (format[i] != '%')
-        {
-            write(1, &format[i], 1);
-            ret++;
-        }
-        if (format[i] == '%')
-        {
-            i++;
-            ret += ft_printer(format[i], &ap);
-        }
-        i++;
-    }
-    va_end(ap);
-    return (ret);
+	ret = 0;
+	i = 0;
+	va_start(ap, format);
+	while(format[i])
+	{
+		if (format[i] != '%')
+		{
+			write(1, &format[i], 1);
+			ret++;
+		}
+		if (format[i] == '%')
+		{
+			i++;
+			ret += ft_printer(format[i], &ap);
+		}
+		i++;
+	}
+	va_end(ap);
+	return (ret);
 }
+
+
+#include <stdio.h>
 
 int main(void)
 {
-    char c = 'c';
-    char *s = "string";
-    int i = -420;
-    int *pi = &i;
-    unsigned int u = 4294967295;
+	char c = 'c';
+	char *s = "string";
+	int i = -420;
+	int *pi = &i;
+	unsigned int u = 4294967295;
+	int ret = 0;
+	unsigned int x = 2319021421;
 
-    printf("\n printf:\n// Conteudo em c:%c\n// Conteudo em *s:%s\n// Conteudo em i:%i\n// Conteudo em u:%u\n// Conteudo em *pi:%p\n", c, s, i, u, pi);
-    ft_printf("\n ft_printf:\n// Conteudo em c:%c\n// Conteudo em *s:%s\n// Conteudo em i:%i\n// Conteudo em u:%u\n", c, s, i, u);
-
-    return(0);
+	printf("\nTESTE PRINTF\n");
+	ret = printf("%%c:%c\n%%s:%s\n%%i/d:%i\n%%u:%u\n%%p:%p\n%%x:%x\n%%X:%X", c, s, i, u, pi, x, x);
+	printf("\nreturn:%i\n", ret);
+	ft_printf("\nTESTE FT_PRINTF\n");
+	ret = ft_printf("%%c:%c\n%%s:%s\n%%i/d:%i\n%%u:%u\n%%p:%p\n%%x:%x\n%%X:%X", c, s, i, u, pi, x, x);
+	ft_printf("\nreturn:%i\n\n", ret);
+	return(0);
 }
